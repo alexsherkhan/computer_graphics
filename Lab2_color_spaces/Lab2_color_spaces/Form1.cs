b86f8eeab0e035e9e0761821cc9af71c1fc06a5e
alivela <42931973+alivela@users.noreply.github.com>
2019-09-13 14:17:47 +0300

    add colour matrix

--- Lab2_color_spaces/Lab2_color_spaces/Form1.cs
+++ Lab2_color_spaces/Lab2_color_spaces/Form1.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,39 +21,31 @@ namespace Lab2_color_spaces
             InitializeComponent();
         }
 
-
-        // Ч/Б
-        private void gray(PictureBox pictureBox1, PictureBox pictureBox2)
-        {
-            if (pictureBox1.Image != null) // если изображение в pictureBox1 имеется
-            {
-                // создаём Bitmap из изображения, находящегося в pictureBox1
-                Bitmap input = new Bitmap(pictureBox1.Image);
-                // создаём Bitmap для черно-белого изображения
-                Bitmap output = new Bitmap(input.Width, input.Height);
-                // перебираем в циклах все пиксели исходного изображения
-                for (int j = 0; j < input.Height; j++)
-                    for (int i = 0; i < input.Width; i++)
-                    {
-                        // получаем (i, j) пиксель
-                        UInt32 pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
-                        // получаем компоненты цветов пикселя
-                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
-                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
-                        float B = (float)(pixel & 0x000000FF); // синий
-                                                               // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
-                        R = G = B = (R + G + B) / 3.0f;
-                        // собираем новый пиксель по частям (по каналам)
-                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
-                        // добавляем его в Bitmap нового изображения
-                        output.SetPixel(i, j, Color.FromArgb((int)newPixel));
-                    }
-                // выводим черно-белый Bitmap в pictureBox2
-                pictureBox2.Image = output;
-
-            }
-        }
-
+        private static ColorMatrix redColour = new ColorMatrix(new float[][] {
+            new float [] {1, 0, 0, 0, 0},
+            new float [] {0, 0, 0, 0, 0 },
+            new float [] {0, 0, 0, 0, 0 },
+            new float [] {0, 0, 0, 1, 0 },
+            new float [] {0, 0, 0, 0, 1 }
+        });
+        private static ColorMatrix greenColour = new ColorMatrix(new float[][] {
+            new float [] {0, 0, 0, 0, 0},
+            new float [] {1, 0, 0, 0, 0 },
+            new float [] {0, 0, 0, 0, 0 },
+            new float [] {0, 0, 0, 1, 0 },
+            new float [] {0, 0, 0, 0, 1 }
+        });
+        private static ColorMatrix blueColour = new ColorMatrix(new float[][] {
+            new float [] {0, 0, 0, 0, 0},
+            new float [] {0, 0, 0, 0, 0 },
+            new float [] {1, 0, 0, 0, 0 },
+            new float [] {0, 0, 0, 1, 0 },
+            new float [] {0, 0, 0, 0, 1 }
+        });
+       
+       // private static Bitmap ConvertColor(Bitmap pic, ColorMatrix transform) {
+            //return;
+      //  }
         private void button1_Click(object sender, EventArgs e)
         {
             // диалог для выбора файла
