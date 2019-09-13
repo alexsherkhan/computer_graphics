6cedbee94f9ba2f4b08b2a94e7c2c39ba312c158
Alexander Syrykh <alexander.syrykh@fast-report.com>
2019-09-13 11:07:11 +0300

    add gray transformition

--- Lab2_color_spaces/Lab2_color_spaces/Form1.cs
+++ Lab2_color_spaces/Lab2_color_spaces/Form1.cs
@@ -16,5 +16,36 @@ namespace Lab2_color_spaces
         {
             InitializeComponent();
         }
+
+        // Ч/Б
+        private void gray(PictureBox pictureBox1, PictureBox pictureBox2)
+        {
+            if (pictureBox1.Image != null) // если изображение в pictureBox1 имеется
+            {
+                // создаём Bitmap из изображения, находящегося в pictureBox1
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                // создаём Bitmap для черно-белого изображения
+                Bitmap output = new Bitmap(input.Width, input.Height);
+                // перебираем в циклах все пиксели исходного изображения
+                for (int j = 0; j < input.Height; j++)
+                    for (int i = 0; i < input.Width; i++)
+                    {
+                        // получаем (i, j) пиксель
+                        UInt32 pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
+                        // получаем компоненты цветов пикселя
+                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                        float B = (float)(pixel & 0x000000FF); // синий
+                                                               // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                        R = G = B = (R + G + B) / 3.0f;
+                        // собираем новый пиксель по частям (по каналам)
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        // добавляем его в Bitmap нового изображения
+                        output.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                // выводим черно-белый Bitmap в pictureBox2
+                pictureBox2.Image = output;
+            }
+        }
     }
 }
