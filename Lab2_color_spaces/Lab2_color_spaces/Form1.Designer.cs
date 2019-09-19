e4ac2a6c6458fc0d9bf6447268992410fc6d7c5b
Alexander Syrykh <alexander.syrykh@fast-report.com>
2019-09-19 22:06:42 +0300

    Fix errors

--- Lab2_color_spaces/Lab2_color_spaces/Form1.Designer.cs
+++ Lab2_color_spaces/Lab2_color_spaces/Form1.Designer.cs
@@ -234,6 +234,7 @@ namespace Lab2_color_spaces
             this.button4.TabIndex = 25;
             this.button4.Text = "RGB -> HSV";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
