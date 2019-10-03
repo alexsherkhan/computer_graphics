f090abfa44f19d17a1d82b26cc4ed1025b914a50
alivela <42931973+alivela@users.noreply.github.com>
2019-10-03 15:08:23 +0300

    add Kramer, the method of corners

---
+++ Lab4_affine_transformations/Lab4_affine_transformations/Primitives/Primitive.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace Lab4_affine_transformations.Primitives
+{
+    interface Primitive
+    {
+        void Draw(Graphics g, bool selected);
+        void Apply(Transformation t);
+    }
+}
