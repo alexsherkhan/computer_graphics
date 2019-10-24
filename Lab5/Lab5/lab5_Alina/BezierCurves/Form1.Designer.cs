c52a2eff23f5e8c913638ed49cf5cd9538601f06
alivela <42931973+alivela@users.noreply.github.com>
2019-10-24 20:48:51 +0300

    Update Form1.Designer.cs

--- Lab5/Lab5/lab5_Alina/BezierCurves/Form1.Designer.cs
+++ Lab5/Lab5/lab5_Alina/BezierCurves/Form1.Designer.cs
@@ -29,78 +29,85 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label label1;
-            this.bezierPlotter1 = new BezierCurves.BezierPlotter();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.bezierPlotter1 = new BezierCurves.BezierPlotter();
             label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Location = new System.Drawing.Point(18, 14);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(112, 13);
+            label1.Size = new System.Drawing.Size(168, 20);
             label1.TabIndex = 1;
             label1.Text = "Клик - создает точку";
-            // 
-            // bezierPlotter1
-            // 
-            this.bezierPlotter1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bezierPlotter1.Location = new System.Drawing.Point(0, 0);
-            this.bezierPlotter1.Name = "bezierPlotter1";
-            this.bezierPlotter1.Size = new System.Drawing.Size(928, 586);
-            this.bezierPlotter1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Location = new System.Drawing.Point(18, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(276, 13);
+            this.label2.Size = new System.Drawing.Size(414, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Клик+перетащить - создаёт точку с направляющими";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Location = new System.Drawing.Point(18, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(344, 13);
+            this.label3.Size = new System.Drawing.Size(516, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "ctrl+перетащить - изменение положения точки или направляющей";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 60);
+            this.label4.Location = new System.Drawing.Point(18, 92);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(135, 13);
+            this.label4.Size = new System.Drawing.Size(206, 20);
             this.label4.TabIndex = 4;
             this.label4.Text = "shift+клик - удалить точку";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 77);
+            this.label5.Location = new System.Drawing.Point(18, 118);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(243, 13);
+            this.label5.Size = new System.Drawing.Size(365, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Пробел - включает/выключает направляющие";
             // 
+            // bezierPlotter1
+            // 
+            this.bezierPlotter1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bezierPlotter1.Location = new System.Drawing.Point(0, 0);
+            this.bezierPlotter1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bezierPlotter1.Name = "bezierPlotter1";
+            this.bezierPlotter1.Size = new System.Drawing.Size(1392, 902);
+            this.bezierPlotter1.TabIndex = 0;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 586);
+            this.ClientSize = new System.Drawing.Size(1392, 902);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(label1);
             this.Controls.Add(this.bezierPlotter1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Кривые Безье";
             this.ResumeLayout(false);
